161dcc6feceac8b6e38393b43ccccc7450ec5a18
Ben Thorne <benjaminjamesthorne@gmail.com>
2024-11-14 07:50:16 +0000

    add invite codes to workspace creation and role permisions to ensure controlled access

---
+++ src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const WorkspaceId = () => {
+  return <div>WorkspaceId</div>;
+};
+
+export default WorkspaceId;
--- src/components/workspace-switcher.tsx
+++ src/components/workspace-switcher.tsx
@@ -12,10 +12,17 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { useRouter } from "next/navigation";
 import { WorkspaceAvatar } from "@/features/workspaces/components/workspace-avatar";
 
 const WorkspaceSwitcher = () => {
   const { data: workspaces } = useGetWorkspaces();
+  const router = useRouter();
+
+  const onSelect = (id: string) => {
+    router.push(`/workspaces/${id}`);
+  };
+
   return (
     <div className="flex flex-col gap-y-2">
       <div className="flex items-center justify-between">
@@ -24,28 +31,30 @@ const WorkspaceSwitcher = () => {
         </p>
         <RiAddCircleFill className="size-5 text-neutral-500 cursor-pointer hover:opacity-75 transition" />
       </div>
-      <Select>
-        <SelectTrigger className="w-full bg-neutral-200 font-medium p-1">
-          <SelectValue placeholder="No workspace selected"></SelectValue>
-        </SelectTrigger>
-        <SelectContent>
-          {workspaces?.documents.map((workspace) => (
-            <SelectItem
-              className="cursor-pointer"
-              value={workspace.$id}
-              key={workspace.$id}
-            >
-              <div className="flex justify-start items-center gap-3 font-medium ">
-                <WorkspaceAvatar
-                  name={workspace.name}
-                  image={workspace.imageUrl}
-                />
-                <span className="truncate">{workspace.name}</span>
-              </div>
-            </SelectItem>
-          ))}
-        </SelectContent>
-      </Select>
+      {workspaces?.total && workspaces?.total > 0 ? (
+        <Select onValueChange={onSelect}>
+          <SelectTrigger className="w-full bg-neutral-200 font-medium p-1">
+            <SelectValue placeholder="No workspace selected"></SelectValue>
+          </SelectTrigger>
+          <SelectContent>
+            {workspaces?.documents.map((workspace) => (
+              <SelectItem
+                className="cursor-pointer"
+                value={workspace.$id}
+                key={workspace.$id}
+              >
+                <div className="flex justify-start items-center gap-3 font-medium ">
+                  <WorkspaceAvatar
+                    name={workspace.name}
+                    image={workspace.imageUrl}
+                  />
+                  <span className="truncate">{workspace.name}</span>
+                </div>
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+      ) : null}
     </div>
   );
 };
