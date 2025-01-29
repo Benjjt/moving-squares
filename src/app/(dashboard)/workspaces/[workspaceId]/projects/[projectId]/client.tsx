4e2b2b91571ab09244a3bdb3bece6cb7489695a5
Ben Thorne <benjaminjamesthorne@gmail.com>
2025-01-29 11:57:06 +0000

    create analytics and add component to projectID page

--- src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/client.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import { Analytics } from "@/components/analytics";
 import { PageError } from "@/components/page-error";
 import { PageLoader } from "@/components/page-loader";
 import { Button } from "@/components/ui/button";
 import { useGetProject } from "@/features/projects/api/use-get-project";
+import { useGetProjectAnalytics } from "@/features/projects/api/use-get-project-analytics";
 import { useGetProjects } from "@/features/projects/api/use-get-projects";
 import { ProjectAvatar } from "@/features/projects/components/project-avatar";
 import { useProjectId } from "@/features/projects/hooks/user-project-id";
@@ -13,13 +15,19 @@ import Link from "next/link";
 
 export const ProjectIdClient = () => {
   const projectId = useProjectId();
-  const { data, isLoading } = useGetProject({ projectId });
+  const { data: project, isLoading: isLoadingProject } = useGetProject({
+    projectId,
+  });
+  const { data: analytics, isLoading: isLoadingAnalytics } =
+    useGetProjectAnalytics({ projectId });
+
+  const isLoading = isLoadingProject || isLoadingAnalytics;
 
   if (isLoading) {
     return <PageLoader />;
   }
 
-  if (!data) {
+  if (!project) {
     return <PageError message="Project not found." />;
   }
 
@@ -28,16 +36,16 @@ export const ProjectIdClient = () => {
       <div className="flex items-center justify-between">
         <div className="flex items-center justify-center gap-x-2">
           <ProjectAvatar
-            name={data.name}
-            image={data.imageUrl}
+            name={project.name}
+            image={project.imageUrl}
             className="size-8"
           />
-          <p className="text-large font-semibold">{data.name}</p>
+          <p className="text-large font-semibold">{project.name}</p>
         </div>
         <div>
           <Button asChild variant="secondary" size="sm">
             <Link
-              href={`/workspaces/${data.workspaceId}/projects/${data.$id}/settings`}
+              href={`/workspaces/${project.workspaceId}/projects/${project.$id}/settings`}
             >
               <PencilIcon className="size-4 mr-2" />
               Edit Project
@@ -45,6 +53,7 @@ export const ProjectIdClient = () => {
           </Button>
         </div>
       </div>
+      {analytics ? <Analytics data={analytics} /> : null}
       <TaskViewSwitcher hideProjectFilter />
     </div>
   );
