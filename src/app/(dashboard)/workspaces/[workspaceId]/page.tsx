b18e2e8346176d1f5b616e7b50752fa31432e3ac
Ben Thorne <benjaminjamesthorne@gmail.com>
2025-01-29 13:03:36 +0000

    complete home page

--- src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
@@ -2,11 +2,12 @@ import React from "react";
 
 import { getCurrent } from "@/features/auth/queries";
 import { redirect } from "next/navigation";
+import { WorkspaceIdClient } from "./client";
 
 const WorkspaceId = async () => {
   const user = await getCurrent();
   if (!user) redirect("/sign-in");
-  return <div>WorkspaceId </div>;
+  return <WorkspaceIdClient />;
 };
 
 export default WorkspaceId;
